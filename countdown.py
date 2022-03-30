7b89d46b723369a718a8cbed600361e2b0b45a92
IkuzoMyDream <96933734+IkuzoMyDream@users.noreply.github.com>
2022-03-30 22:51:03 +0700

    Create countdown.py

---
+++ countdown.py
@@ -0,0 +1,14 @@
+# 6
+import time
+def countdown(t):
+    while t:
+        mins, secs = divmod(t, 60) # divmid - ผลหารไม่่เอาเศษ
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end ="\r")
+        time.sleep(1)
+        t -= 1
+
+    print('Timer completed!!!')
+
+t = input('Enter the time in seconds: ')
+countdown(int(t))
